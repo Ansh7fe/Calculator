90e64832979d0f3a5665e54390b8d1fbad94b866
Ansh Jhanwar <lordansh07@gmial.com>
2022-01-11 11:15:06 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// initializing variable
+let display=document.getElementById("screen");
+let buttons=document.querySelectorAll("button");
+let screenValue="";
+for(item of buttons){
+   item.addEventListener("click",(e)=>{
+       buttonTxt=e.target.innerText;
+       console.log("button is",buttonTxt);
+       if(buttonTxt=='X'){
+           buttonTxt="*";
+           screenValue+=buttonTxt;
+           display.value=screenValue ;
+       }
+       else if(buttonTxt=='C'){
+           screenValue=""
+           display.value=screenValue;
+       }
+        else if(buttonTxt=='='){
+           display.value=eval(screenValue);
+       }
+       else{
+        screenValue+=buttonTxt;
+        display.value=(screenValue) ;
+       }
+   })
+}
